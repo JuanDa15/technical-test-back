a8777ddcc04605a10b7e1bdb7ee0bd3727eaf983
Juan David Osorio Ortiz <jdoo1115@gmail.com>
2023-07-17 17:44:40 -0500

    test

--- src/index.js
+++ src/index.js
@@ -63,7 +63,8 @@ app.use('/api/hotels', require('./routes/hotel.routes'))
 app.use('/api/rooms', require('./routes/room.routes'))
 app.use('/api/reservations', require('./routes/reservation.routes'))
 app.listen(process.env.PORT || 80, () => {
-  console.log('Runing in port', process.env.PORT)
+  console.log('Runing in port', process.env.PORT);
+  console.log(process.env.EMAIL_PASS)
 })
 
 // POPULATE: TRAE LA REFERENENCIA DE LAS TABLAS
